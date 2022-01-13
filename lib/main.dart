4550c7733ff9a87e65ad9483196d3c26a038152d
Alex Chinedu <nwekealexchinedu@gmail.com>
2022-01-13 16:54:40 +0100

    changed the text theme to white

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,11 @@ class BMICalculator extends StatelessWidget {
         appBarTheme: AppBarTheme(color: Color(0xFF0A0E21)),
         scaffoldBackgroundColor: Color(0xFF0A0E21),
         primaryColor: Color(0xFF0A0E21),
+        textTheme: TextTheme(
+          bodyText2: TextStyle(
+            color: Colors?.white,
+          ),
+        ),
         colorScheme: ColorScheme.fromSwatch().copyWith(
           secondary: Colors.purple,
         ),
