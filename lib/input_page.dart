9ab685015c39ca9b2a06467111bdaaa6f1f0a334
Alex Chinedu <nwekealexchinedu@gmail.com>
2022-01-13 19:21:00 +0100

    gave the bottom container a constant height

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,7 @@
 import "package:flutter/material.dart";
 
+const bottomContainerHeight = 80.0;
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -57,7 +59,7 @@ class _InputPageState extends State<InputPage> {
               color: Color(0xFFEB1555),
               margin: EdgeInsets.only(top: 15.0),
               width: double.infinity,
-              height: 80.0,
+              height: bottomContainerHeight,
             )
           ],
         ));
