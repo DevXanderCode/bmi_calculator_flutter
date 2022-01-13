322a14691e03cba52edf68845f1542b7b8915540
Alex Chinedu <nwekealexchinedu@gmail.com>
2022-01-13 16:59:36 +0100

    extended the dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -6,18 +6,10 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
+      theme: ThemeData.dark().copyWith(
         appBarTheme: AppBarTheme(color: Color(0xFF0A0E21)),
         scaffoldBackgroundColor: Color(0xFF0A0E21),
         primaryColor: Color(0xFF0A0E21),
-        textTheme: TextTheme(
-          bodyText2: TextStyle(
-            color: Colors?.white,
-          ),
-        ),
-        colorScheme: ColorScheme.fromSwatch().copyWith(
-          secondary: Colors.purple,
-        ),
       ),
       home: InputPage(),
     );
