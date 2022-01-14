da5aa4ace50ee21b69c7c566106ea96df100cbac
Alex Chinedu <nwekealexchinedu@gmail.com>
2022-01-14 10:02:05 +0100

    added the slider

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -14,6 +14,7 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
   Gender selectedGender;
+  int height = 180;
 
   @override
   Widget build(BuildContext context) {
@@ -67,7 +68,7 @@ class _InputPageState extends State<InputPage> {
               child: ReusableCard(
                 colour: kActiveCardColor,
                 cardChild: Column(
-                  // mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     Text(
                       "HEIGHT:",
@@ -75,14 +76,31 @@ class _InputPageState extends State<InputPage> {
                     ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.baseline,
+                      textBaseline: TextBaseline.alphabetic,
                       children: <Widget>[
                         Text(
-                          '180',
+                          height.toString(),
                           style: kNumberTextStyle,
                         ),
-                        Text('cm'),
+                        Text(
+                          'cm',
+                          style: kLabelTextStyle,
+                        ),
                       ],
-                    )
+                    ),
+                    Slider(
+                      value: height.toDouble(),
+                      min: 120.0,
+                      max: 220.0,
+                      activeColor: Color(0xFFEB1555),
+                      inactiveColor: Color(0xFF8D8E98),
+                      onChanged: (double newValue) {
+                        setState(() {
+                          height = newValue.toInt();
+                        });
+                      },
+                    ),
                   ],
                 ),
               ),
